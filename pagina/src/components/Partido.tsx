43d92e2c4d2172bce7e07b7fb6d2ac44de056042
emiliomtzr <emilio400g@gmail.com>
2026-04-07 23:24:12 -0600

    cambio a localhost
    para mis pruebas (aysi)

--- pagina/src/components/Partido.tsx
+++ pagina/src/components/Partido.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-const API_URL: string = "https://api.zamer-o.com";
+const API_URL: string = "http://localhost:4000";
 
 interface ApiMatch {
   fixture: { id: number; date: string; status: { short: string; long: string } };
